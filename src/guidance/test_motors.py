b566764fa729ad79e8d5b78a195128a525f840a1
Ernesti Sario <56239254+BitBeholder@users.noreply.github.com>
2024-04-13 17:12:59 +0300

    test motors changes

--- src/guidance/test_motors.py
+++ src/guidance/test_motors.py
@@ -5,52 +5,38 @@ import time
 GPIO.setmode(GPIO.BCM)  # Use BCM numbering
 GPIO.setwarnings(False)
 
-# Define GPIO pins for Motor 1
-Motor1A = 17  # Input A
-Motor1B = 18  # Input B (Not used for direction)
-Motor1E = 27  # Enable A
-
-# Define GPIO pins for Motor 2
-Motor2A = 22  # Input A
-Motor2B = 23  # Input B (Not used for direction)
-Motor2E = 24  # Enable B
+# Define GPIO pins for Motor 1 and Motor 2
+Motor1 = 17  # Pin connected to the first motor
+Motor2 = 22  # Pin connected to the second motor
 
 # Set up the motor pins:
-GPIO.setup(Motor1A, GPIO.OUT)
-GPIO.setup(Motor1B, GPIO.OUT)
-GPIO.setup(Motor1E, GPIO.OUT)
-GPIO.setup(Motor2A, GPIO.OUT)
-GPIO.setup(Motor2B, GPIO.OUT)
-GPIO.setup(Motor2E, GPIO.OUT)
+GPIO.setup(Motor1, GPIO.OUT)
+GPIO.setup(Motor2, GPIO.OUT)
 
 # Function to turn on a motor
-def motor_on(motor, enable):
-    GPIO.output(motor[0], True)
-    GPIO.output(motor[1], False)  # Not changing the direction, just ensuring it is off
-    GPIO.output(enable, True)
+def motor_on(pin):
+    GPIO.output(pin, True)
 
 # Function to turn off a motor
-def motor_off(motor, enable):
-    GPIO.output(motor[0], False)
-    GPIO.output(motor[1], False)
-    GPIO.output(enable, False)
+def motor_off(pin):
+    GPIO.output(pin, False)
 
 try:
     while True:
         # Turn Motor 1 on
-        motor_on((Motor1A, Motor1B), Motor1E)
+        motor_on(Motor1)
         time.sleep(2)
 
         # Turn Motor 1 off
-        motor_off((Motor1A, Motor1B), Motor1E)
+        motor_off(Motor1)
         time.sleep(2)
 
         # Turn Motor 2 on
-        motor_on((Motor2A, Motor2B), Motor2E)
+        motor_on(Motor2)
         time.sleep(2)
 
         # Turn Motor 2 off
-        motor_off((Motor2A, Motor2B), Motor2E)
+        motor_off(Motor2)
         time.sleep(2)
 
 except KeyboardInterrupt:
