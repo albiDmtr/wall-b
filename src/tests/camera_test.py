b4a64a837279bce3967ac4c2c7fe8c5bc562997c
Ernesti Sario <56239254+BitBeholder@users.noreply.github.com>
2024-04-13 17:40:06 +0300

    changed video device to 1

---
+++ src/tests/camera_test.py
@@ -0,0 +1,28 @@
+import cv2
+
+# Capture video from the first USB webcam detected
+cap = cv2.VideoCapture(1)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # If frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Webcam Live', frame)
+
+    # Press 'q' to quit the window
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
