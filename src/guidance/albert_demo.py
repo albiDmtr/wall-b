78f15fe817970e06ca16a751e5efb5dde67125fd
Albert Domotor <domotor.albert@gmail.com>
2024-04-13 17:17:35 +0300

    Update albert_demo.py

--- src/guidance/albert_demo.py
+++ src/guidance/albert_demo.py
@@ -4,15 +4,15 @@ import time
 left_wheel_pin = 17  # GPIO pin you are using
 right_wheel_pin = 22
 GPIO.setmode(GPIO.BCM)  # Use Broadcom pin-numbering scheme
-GPIO.setup(left_wheel_pin, GPIO.OUT)  # Set GPIO pin to output mode
-GPIO.setup(right_wheel_pin, GPIO.OUT)  # Set GPIO pin to output mode
 
 while True:
+    GPIO.setup(left_wheel_pin, GPIO.OUT)
     GPIO.output(left_wheel_pin, True)
     time.sleep(2)
-    GPIO.output(right_wheel_pin, False)
-    time.sleep(2)
-    GPIO.output(left_wheel_pin, True)
+    GPIO.output(left_wheel_pin, False)
+
+    GPIO.setup(right_wheel_pin, GPIO.OUT)
+    GPIO.output(right_wheel_pin, True)
     time.sleep(2)
     GPIO.output(right_wheel_pin, False)
     time.sleep(2)
