128810f7bb9b70c481492f545022da4ab480fc78
Albert Domotor <domotor.albert@gmail.com>
2024-04-13 18:55:05 +0300

    Update buildit_demo.py

--- src/guidance/buildit_demo.py
+++ src/guidance/buildit_demo.py
@@ -5,11 +5,12 @@ left_wheel_pin = 17  # GPIO pin you are using
 right_wheel_pin = 22
 GPIO.setmode(GPIO.BCM)  # Use Broadcom pin-numbering scheme
 
-GPIO.setup(left_wheel_pin, GPIO.OUT)
-GPIO.setup(right_wheel_pin, GPIO.OUT)
 time.sleep(1)  # Sleep for 1 second
 
+GPIO.setup(left_wheel_pin, GPIO.OUT)
 GPIO.output(left_wheel_pin, True)
+
+GPIO.setup(right_wheel_pin, GPIO.OUT)
 while True:
     # circle
 
