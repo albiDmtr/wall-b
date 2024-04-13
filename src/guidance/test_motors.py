12f3f5f47e288068757b7a0497ef0c829b2395b4
Ernesti Sario <56239254+BitBeholder@users.noreply.github.com>
2024-04-13 17:00:59 +0300

    test motors edit

--- src/guidance/test_motors.py
+++ src/guidance/test_motors.py
@@ -7,12 +7,12 @@ GPIO.setwarnings(False)
 
 # Define GPIO pins for Motor 1
 Motor1A = 17  # Input A
-Motor1B = 18  # Input B
+Motor1B = 18  # Input B (Not used for direction)
 Motor1E = 27  # Enable A
 
 # Define GPIO pins for Motor 2
 Motor2A = 22  # Input A
-Motor2B = 23  # Input B
+Motor2B = 23  # Input B (Not used for direction)
 Motor2E = 24  # Enable B
 
 # Set up the motor pins:
@@ -23,43 +23,34 @@ GPIO.setup(Motor2A, GPIO.OUT)
 GPIO.setup(Motor2B, GPIO.OUT)
 GPIO.setup(Motor2E, GPIO.OUT)
 
-# Function to run a motor
-def motor_forward(motor, enable):
+# Function to turn on a motor
+def motor_on(motor, enable):
     GPIO.output(motor[0], True)
-    GPIO.output(motor[1], False)
+    GPIO.output(motor[1], False)  # Not changing the direction, just ensuring it is off
     GPIO.output(enable, True)
 
-def motor_reverse(motor, enable):
-    GPIO.output(motor[0], False)
-    GPIO.output(motor[1], True)
-    GPIO.output(enable, True)
-
-def motor_stop(motor, enable):
+# Function to turn off a motor
+def motor_off(motor, enable):
     GPIO.output(motor[0], False)
     GPIO.output(motor[1], False)
     GPIO.output(enable, False)
 
 try:
     while True:
-        # Motor 1 forward
-        motor_forward((Motor1A, Motor1B), Motor1E)
+        # Turn Motor 1 on
+        motor_on((Motor1A, Motor1B), Motor1E)
         time.sleep(2)
 
-        # Motor 1 reverse
-        motor_reverse((Motor1A, Motor1B), Motor1E)
+        # Turn Motor 1 off
+        motor_off((Motor1A, Motor1B), Motor1E)
         time.sleep(2)
 
-        # Motor 2 forward
-        motor_forward((Motor2A, Motor2B), Motor2E)
+        # Turn Motor 2 on
+        motor_on((Motor2A, Motor2B), Motor2E)
         time.sleep(2)
 
-        # Motor 2 reverse
-        motor_reverse((Motor2A, Motor2B), Motor2E)
-        time.sleep(2)
-
-        # Stop Motors
-        motor_stop((Motor1A, Motor1B), Motor1E)
-        motor_stop((Motor2A, Motor2B), Motor2E)
+        # Turn Motor 2 off
+        motor_off((Motor2A, Motor2B), Motor2E)
         time.sleep(2)
 
 except KeyboardInterrupt:
