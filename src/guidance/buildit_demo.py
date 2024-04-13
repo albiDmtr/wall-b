03a1931b5bf76c4c3a31c1e8452439a2e297a7b4
Albert Domotor <domotor.albert@gmail.com>
2024-04-13 18:49:06 +0300

    Create buildit_demo.py

---
+++ src/guidance/buildit_demo.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+left_wheel_pin = 17  # GPIO pin you are using
+right_wheel_pin = 22
+GPIO.setmode(GPIO.BCM)  # Use Broadcom pin-numbering scheme
+
+GPIO.setup(left_wheel_pin, GPIO.OUT)
+GPIO.setup(right_wheel_pin, GPIO.OUT)
+time.sleep(1)  # Sleep for 1 second
+
+while True:
+    # circle
+
+    # left turn
+    GPIO.output(left_wheel_pin, True)
+    time.sleep(0.5)
+
+    # straight
+    GPIO.output(right_wheel_pin, True)
+    time.sleep(0.5)
+    GPIO.output(right_wheel_pin, False)
+
+
+
+GPIO.cleanup()
