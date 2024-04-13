531e516b685d386585f9fca6b2c6da1ec15a5caa
Albert Domotor <domotor.albert@gmail.com>
2024-04-13 19:00:38 +0300

    Update buildit_demo.py

--- src/guidance/buildit_demo.py
+++ src/guidance/buildit_demo.py
@@ -1,41 +1,12 @@
-import RPi.GPIO as GPIO
-import time
+from gpiozero import LED
+from time import sleep
 
-# Pin Definitions
-pin_stay_on = 17  # GPIO pin to stay on constantly
-pin_blink = 22   # GPIO pin to blink
+left_wheel = LED(17)
+right_wheel = LED(22)
 
-# Setup
-GPIO.setmode(GPIO.BCM)  # Use Broadcom pin-numbering scheme
-GPIO.setwarnings(False)  # Disable warnings to avoid clutter
-
-# Setup Pins
-GPIO.setup(pin_stay_on, GPIO.OUT)
-GPIO.setup(pin_blink, GPIO.OUT)
-
-# Turn on the pin_stay_on continuously
-GPIO.output(pin_stay_on, True)
-print(f"GPIO pin {pin_stay_on} is on continuously.")
-
-# Blinking Loop for pin_blink
-try:
-    while True:
-        # Turn on pin_blink
-        GPIO.output(pin_blink, True)
-        print(f"GPIO pin {pin_blink} is on.")
-        time.sleep(0.5)  # On for 0.5 seconds
-
-        # Turn off pin_blink
-        GPIO.output(pin_blink, False)
-        print(f"GPIO pin {pin_blink} is off.")
-        time.sleep(0.5)  # Off for 0.5 seconds
-
-except KeyboardInterrupt:
-    # Clean up GPIO settings when the user hits Ctrl+C
-    print("Exiting program.")
-    GPIO.cleanup()
-
-except:
-    # Clean up GPIO settings on any other error.
-    print("An error occurred. Cleaning up...")
-    GPIO.cleanup()
+right_wheel.on()
+while True:
+    left_wheel.on()
+    sleep(0.5)
+    left_wheel.off()
+    sleep(0.5)
