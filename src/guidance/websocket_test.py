a4c7c622ea98783dbc3f01a46106ce4d5bf6f5a8
Ernesti Sario <56239254+BitBeholder@users.noreply.github.com>
2024-05-11 16:13:15 +0300

    websocket edit pynput

--- src/guidance/websocket_test.py
+++ src/guidance/websocket_test.py
@@ -1,40 +1,39 @@
+from pynput.keyboard import Key, Listener
 import RPi.GPIO as GPIO
-import pygame
 
-# Pin setup
+# GPIO setup
 left_wheel_pin = 17
 right_wheel_pin = 22
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(left_wheel_pin, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(right_wheel_pin, GPIO.OUT, initial=GPIO.HIGH)
 
-# Pygame setup
-pygame.init()
-size = (10, 10)  # Small window
-screen = pygame.display.set_mode(size, pygame.NOFRAME)
-pygame.display.set_caption("Wheel Control Test")
+def on_press(key):
+    try:
+        if key == Key.right:
+            GPIO.output(left_wheel_pin, GPIO.LOW)
+            print("Left wheel is ON")
+        elif key == Key.left:
+            GPIO.output(right_wheel_pin, GPIO.LOW)
+            print("Right wheel is ON")
+        elif key == Key.up:
+            GPIO.output(left_wheel_pin, GPIO.LOW)
+            GPIO.output(right_wheel_pin, GPIO.LOW)
+            print("Both wheels are ON")
+    except AttributeError:
+        pass
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                GPIO.output(left_wheel_pin, GPIO.LOW)
-                print("Left wheel is ON")
-            elif event.key == pygame.K_LEFT:
-                GPIO.output(right_wheel_pin, GPIO.LOW)
-                print("Right wheel is ON")
-            elif event.key == pygame.K_UP:
-                GPIO.output(left_wheel_pin, GPIO.LOW)
-                GPIO.output(right_wheel_pin, GPIO.LOW)
-                print("Both wheels are ON")
-        elif event.type == pygame.KEYUP:
-            if event.key in [pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP]:
-                GPIO.output(left_wheel_pin, GPIO.HIGH)
-                GPIO.output(right_wheel_pin, GPIO.HIGH)
-                print("Wheels are OFF")
+def on_release(key):
+    if key in [Key.right, Key.left, Key.up]:
+        GPIO.output(left_wheel_pin, GPIO.HIGH)
+        GPIO.output(right_wheel_pin, GPIO.HIGH)
+        print("Wheels are OFF")
+    if key == Key.esc:  # Press escape to stop the listener
+        return False
 
-pygame.quit()
+# Collect events until released
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
 GPIO.cleanup()  # Clean up GPIO on closing
+
