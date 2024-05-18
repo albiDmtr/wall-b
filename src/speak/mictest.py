71ae15ab614b52ec5685c3aa1c8d1e175d638ecc
bitbeholder <e.sario24@gmail.com>
2024-05-18 22:41:06 +0300

    cleaning the repo

--- src/speak/mictest.py
+++
@@ -1,4 +0,0 @@
-import speech_recognition as sr
-import pyaudio
-for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    print(f"Microphone with name \"{name}\" found for Microphone(device_index={index})")
--- src/speak/pygame_audio_test.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-pygame.mixer.init()
-pygame.mixer.music.load('/home/wallb/Desktop/wall-b/src/speak/sample-6s.mp3')
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy():
-	pygame.time.Clock().tick(10)
