5fe8f0f8198de2785b3d7d01f20e83a1d330fee6
Ernesti Sario <56239254+BitBeholder@users.noreply.github.com>
2024-05-26 08:42:38 +0300

    moving backwards, added pins and key presses

--- src/programs/remote_control.py
+++ src/programs/remote_control.py
@@ -3,10 +3,14 @@ import RPi.GPIO as GPIO
 
 # GPIO setup
 left_wheel_pin = 17
+left_wheel_pin_back = 27
 right_wheel_pin = 22
+right_wheel_pin_back = 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(left_wheel_pin, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(right_wheel_pin, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(left_wheel_pin_back, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(right_wheel_pin_back, GPIO.OUT, initial=GPIO.HIGH)
 
 # Specify the device path for the keyboard (find it using `ls /dev/input/by-id/`)
 keyboard = InputDevice('/dev/input/event0')  # Adjust event number as necessary
@@ -25,8 +29,11 @@ def handle_key_press(key_event):
                 GPIO.output(left_wheel_pin, GPIO.LOW)
                 GPIO.output(right_wheel_pin, GPIO.LOW)
                 print("Both wheels are ON")
+            elif key_event.keycode == 'KEY_DOWN':
+                GPIO.output(left_wheel_pin_back, GPIO.LOW)
+                GPIO.output(right_wheel_pin_back, GPIO.LOW)
         elif key_event.keystate == key_event.key_up:
-            if key_event.keycode in ['KEY_RIGHT', 'KEY_LEFT', 'KEY_UP']:
+            if key_event.keycode in ['KEY_RIGHT', 'KEY_LEFT', 'KEY_UP', 'KEY_DOWN']:
                 GPIO.output(left_wheel_pin, GPIO.HIGH)
                 GPIO.output(right_wheel_pin, GPIO.HIGH)
                 print("Wheels are OFF")
