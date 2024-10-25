00bb79c3b7489a841eb5c1a352e69a455c13c02a
Albert Domotor <hello@albertdomotor.ninja>
2024-10-25 19:17:41 +0300

    Create test_distance.py

---
+++ src/test_distance.py
@@ -0,0 +1,39 @@
+import lgpio as GPIO
+import time
+
+TRIG = 31
+ECHO = 29
+
+h = GPIO.gpiochip_open(0)
+GPIO.claim_output(h, TRIG)
+GPIO.claim_input(h, ECHO)
+
+def get_distance():
+    GPIO.write(h, TRIG, 0)
+    time.sleep(2)
+
+    GPIO.gpio_write(h, TRIG, 1)
+    time.sleep(0.00001)
+    GPIO.gpio_write(h, TRIG, 0)
+
+    while GPIO.gpio_read(h, ECHO) == 0:
+        pulse_start = time.time()
+    
+    while GPIO.gpio_read(h, ECHO) == 1:
+        pulse_end = time.time()
+    
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+
+    return distance
+
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = get_distance()
+            print(f"Distance: {dist} cm")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        GPIO.gpiochip_close(h)
+        print("Exiting...")
