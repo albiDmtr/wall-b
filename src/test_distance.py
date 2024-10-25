8b9b06bf6e66910ce1c80a8d7b3831db3531078b
Albert Domotor <hello@albertdomotor.ninja>
2024-10-25 23:58:12 +0300

    Update test_distance.py

--- src/test_distance.py
+++ src/test_distance.py
@@ -1,39 +1,49 @@
-import lgpio as GPIO
+import RPi.GPIO as GPIO
 import time
 
-TRIG = 6
-ECHO = 5
 
-h = GPIO.gpiochip_open(0)
-GPIO.gpio_claim_output(h, TRIG)
-GPIO.gpio_claim_input(h, ECHO)
+GPIO.setmode(GPIO.BOARD)
 
-def get_distance():
-    GPIO.gpio_write(h, TRIG, 0)
-    time.sleep(2)
+TRIG = 16
+ECHO = 18
+i=0
 
-    GPIO.gpio_write(h, TRIG, 1)
-    time.sleep(0.00001)
-    GPIO.gpio_write(h, TRIG, 0)
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
 
-    while GPIO.gpio_read(h, ECHO) == 0:
-        pulse_start = time.time()
-    
-    while GPIO.gpio_read(h, ECHO) == 1:
-        pulse_end = time.time()
-    
-    pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150
-    distance = round(distance, 2)
+GPIO.output(TRIG, False)
+print("Calibrating.....")
+time.sleep(2)
 
-    return distance
+print("Place the object......")
 
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = get_distance()
-            print(f"Distance: {dist} cm")
-            time.sleep(1)
-    except KeyboardInterrupt:
-        GPIO.gpiochip_close(h)
-        print("Exiting...")
+
+try:
+    while True:
+       GPIO.output(TRIG, True)
+       time.sleep(0.00001)
+       GPIO.output(TRIG, False)
+
+       while GPIO.input(ECHO)==0:
+          pulse_start = time.time()
+
+       while GPIO.input(ECHO)==1:
+          pulse_end = time.time()
+
+       pulse_duration = pulse_end - pulse_start
+
+       distance = pulse_duration * 17150
+
+       distance = round(distance+1.15, 2)
+  
+       if distance<=20 and distance>=5:
+          print(f"distance: {distance}cm")
+          i=1
+          
+       if distance>20 and i==1:
+          print("place the object....")
+          i=0
+       time.sleep(2)
+
+except KeyboardInterrupt:
+     GPIO.cleanup()
